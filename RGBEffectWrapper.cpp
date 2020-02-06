3225627b82544e00e50a5d6f21d5d59ff21b4b41
plugz <david.garyga@gmail.com>
2020-02-06 18:03:46 +0100

    back2basic

--- RGBEffectWrapper.cpp
+++ RGBEffectWrapper.cpp
@@ -5,10 +5,10 @@
 #define MYMIN(x, y) ((x) < (y) ? (x) : (y))
 
 const RGBEffect::PosArray RGBEffectWrapper::posArray =
-/** /
-    RGBEffect::posArraySimple(CFG_LED_COUNT, 1);
-/ **/
 /**/
+    RGBEffect::posArraySimple(CFG_LED_COUNT, 1);
+/**/
+/** /
     RGBEffect::posArrayFromLedArray({// Fairy Wings V2
         -1, -1, -1, -1, -1, 14, -1, 15, -1, 16, -1, -1, -1, -1, -1, -1, -1, -1, 23, -1, 24, -1, 25, -1, -1, -1, -1, -1,
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 26, -1, -1, -1,
@@ -35,7 +35,7 @@ const RGBEffect::PosArray RGBEffectWrapper::posArray =
         -1, -1, -1, -1, -1, -1, -1, -1,  3, -1, -1, -1, -1, -1,  0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  2, -1,  1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
         }, 28, 24);
-/**/
+/ **/
 /** /
     RGBEffect::posArrayFromLedArray({// Eventail
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 17, -1, 18, -1, 19, -1, 20, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
